9108c1bd5d94b30a9650a6bffff8479271733fe8
Vasiliy Zhuk <vasyares@gmail.com>
2015-02-19 16:49:11 +0300

    Initial Commit

---
+++ HAProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HAProject
+//
+//  Created by Vasiliy Zhuk on 2/19/15.
+//  Copyright (c) 2015 Vasiliy Zhuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
